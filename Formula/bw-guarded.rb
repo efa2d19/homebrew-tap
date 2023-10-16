48c62f65fad074b06e6eed53738fb185fbee7e88
drugsosos <44712637+Drugsosos@users.noreply.github.com>
2023-10-16 22:56:37 +0400

    Init bw-guarded.rb

---
+++ Formula/bw-guarded.rb
@@ -0,0 +1,27 @@
+class BwGuarded < Formula
+  version "1.0.0"
+  desc "Bitwarden CLI overhead to make it guarded with root password and GPG"
+  homepage "https://github.com/Drugsosos/bw-guarded"
+  url "https://github.com/Drugsosos/bw-guarded/archive/refs/tags/#{version}.tar.gz"
+  sha256 "85779750025385cbf808dffe10fb262893e71e9e713190a5993e16498958635e"
+
+  depends_on "gnupg"
+  depends_on "bitwarden-cli"
+
+  def install
+    bin.install "bw-guarded" => "bw"
+  end
+
+  test do
+  	assert_match "bw --generate-session-key", shell_output("bw-guarded --help")
+  end
+
+  def caveats
+      <<~EOS
+        Unlink bitwarden-cli if you see errors above
+        Now you can use bitwarden-cli as usual
+        But run this first to init and encrypt session
+          bw --generate-session-key
+      EOS
+    end
+end
