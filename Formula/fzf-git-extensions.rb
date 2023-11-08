1a8028cb1bff85465d1aa833310af1b463e24a59
drugsosos <44712637+Drugsosos@users.noreply.github.com>
2023-11-08 18:19:34 +0400

    bump(fzf-git-extensions): to 1.0.5

--- Formula/fzf-git-extensions.rb
+++ Formula/fzf-git-extensions.rb
@@ -1,9 +1,9 @@
 class FzfGitExtensions < Formula
-  version "1.0.4"
+  version "1.0.5"
   desc "Quick Git actions using fzf"
   homepage "https://github.com/Drugsosos/fzf-git-extensions"
   url "https://github.com/Drugsosos/fzf-git-extensions/archive/refs/tags/#{version}.tar.gz"
-  sha256 "5a7fcbd3a73d93be36cad6e8b3572c79ab1657a5c0e3146baf25808cb5220b69"
+  sha256 "4369aaac49d862dbe6797c6491f209c05a2ce8018e5b1696488a3fbc806290a9"
 
   depends_on "fzf"
   depends_on "git-delta" => :recommended
