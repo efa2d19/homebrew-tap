f71d1d5f17e892e508ca2636e5eed77871c40a92
Sasha <44712637+efa2d19@users.noreply.github.com>
2024-04-08 22:20:16 +0400

    fix(httpServe): updated homepage & url after nickname change

--- Formula/httpServe.rb
+++ Formula/httpServe.rb
@@ -3,8 +3,8 @@ class Httpserve < Formula
   
   version "1.0.0"
   desc "http.server but with uploads"
-  homepage "https://github.com/Drugsosos/httpServe"
-  url "https://github.com/Drugsosos/httpServe/archive/refs/tags/#{version}.tar.gz"
+  homepage "https://github.com/efa2d19/httpServe"
+  url "https://github.com/efa2d19/httpServe/archive/refs/tags/#{version}.tar.gz"
   sha256 "3918e764ac3846b67e97c28a3e1b58350cee38d8465506c54cfe11008234e3fb"
 
   depends_on "python@3.12"
