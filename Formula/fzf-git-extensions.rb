1fd33bb7433062e8da0834710468ab3226be5dc9
Sasha <44712637+efa2d19@users.noreply.github.com>
2024-05-06 17:20:42 +0400

    bump(fzf-git-extensions): 1.1.0

--- Formula/fzf-git-extensions.rb
+++ Formula/fzf-git-extensions.rb
@@ -1,9 +1,9 @@
 class FzfGitExtensions < Formula
-  version "1.0.8"
+  version "1.1.0"
   desc "Quick Git actions using fzf"
   homepage "https://github.com/efa2d19/fzf-git-extensions"
   url "https://github.com/efa2d19/fzf-git-extensions/archive/refs/tags/#{version}.tar.gz"
-  sha256 "95f35deb14792d7029dd96f8fdc4e0711b93ac11ee3081e44182cdc7427d49d7"
+  sha256 "c6e73b66b590642b3157d163ac42b67c5c292a9ebd42d6a13d3851f90aed2268"
 
   depends_on "fzf"
   depends_on "git-delta" => :recommended
