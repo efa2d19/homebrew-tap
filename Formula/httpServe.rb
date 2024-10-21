74b1991763b3973122374323808ffc66ca696407
Sasha <44712637+efa2d19@users.noreply.github.com>
2024-10-21 16:31:29 +0400

    build(httpServe): bumped to 1.1.0

--- Formula/httpServe.rb
+++ Formula/httpServe.rb
@@ -1,16 +1,16 @@
 class Httpserve < Formula
   include Language::Python::Virtualenv
   
-  version "1.0.0"
+  version "1.1.0"
   desc "http.server but with uploads"
   homepage "https://github.com/efa2d19/httpServe"
   url "https://github.com/efa2d19/httpServe/archive/refs/tags/#{version}.tar.gz"
-  sha256 "3918e764ac3846b67e97c28a3e1b58350cee38d8465506c54cfe11008234e3fb"
+  sha256 "4b412bedc9b64509a3c35e2f924f8c0572fae3539218a6199d727ef66b84f508"
 
-  depends_on "python@3.12"
+  depends_on "python@3.10"
 
   def install
-    bin.install "httpServe"
+    bin.install "httpserve"
   end
 
   def caveats
