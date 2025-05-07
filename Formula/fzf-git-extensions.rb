03f0e152def984d07542bda9de88b01633ecb524
Sasha <44712637+efa2d19@users.noreply.github.com>
2025-05-07 14:27:25 +0400

    chore(fzf-git-extensions): updated to 1.2.0

--- Formula/fzf-git-extensions.rb
+++ Formula/fzf-git-extensions.rb
@@ -1,18 +1,14 @@
 class FzfGitExtensions < Formula
-  version "1.1.2"
+  version "1.2.0"
   desc "Quick Git actions using fzf"
   homepage "https://github.com/efa2d19/fzf-git-extensions"
   url "https://github.com/efa2d19/fzf-git-extensions/archive/refs/tags/#{version}.tar.gz"
-  sha256 "db9f7334f7a6d5ca1b1290accd2f446537e8c93e9cf54d75e6a90d02695f5ddc"
+  sha256 "1bdde71c95a278507d7c2e2b6b753f3a9764a6466a623c57eb603349afd3a341"
 
   depends_on "fzf"
   depends_on "git-delta" => :recommended
-  depends_on "bat" => :recommended
-  depends_on "micro" => :recommended
 
   def install
-    bin.install "bati"
-    bin.install "microi"
     bin.install "gdi"
     bin.install "gdsi"
     bin.install "gsti"
