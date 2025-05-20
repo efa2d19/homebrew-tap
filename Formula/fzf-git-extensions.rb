9a65a155807c0985be1102435f83935d7c54743a
Sasha <44712637+efa2d19@users.noreply.github.com>
2025-05-20 18:24:48 +0400

    chore(fzf-git-extensions): updated to 1.2.1

--- Formula/fzf-git-extensions.rb
+++ Formula/fzf-git-extensions.rb
@@ -1,9 +1,9 @@
 class FzfGitExtensions < Formula
-  version "1.2.0"
+  version "1.2.1"
   desc "Quick Git actions using fzf"
   homepage "https://github.com/efa2d19/fzf-git-extensions"
   url "https://github.com/efa2d19/fzf-git-extensions/archive/refs/tags/#{version}.tar.gz"
-  sha256 "1bdde71c95a278507d7c2e2b6b753f3a9764a6466a623c57eb603349afd3a341"
+  sha256 "6e7d2d1dec41e380e9ef435d21bdbc9eccb513c1c113a6319722abaec1720353"
 
   depends_on "fzf"
   depends_on "git-delta" => :recommended
