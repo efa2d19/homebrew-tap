ac77fd8eb5a98a44075ba4375fb6e3367d423b73
efa2d19 <44712637+efa2d19@users.noreply.github.com>
2025-06-15 00:49:04 +0400

    chore(fzf-git-extensions): bump to 1.3.0

--- Formula/fzf-git-extensions.rb
+++ Formula/fzf-git-extensions.rb
@@ -1,12 +1,13 @@
 class FzfGitExtensions < Formula
-  version "1.2.1"
+  version "1.3.0"
   desc "Quick Git actions using fzf"
   homepage "https://github.com/efa2d19/fzf-git-extensions"
   url "https://github.com/efa2d19/fzf-git-extensions/archive/refs/tags/#{version}.tar.gz"
-  sha256 "6e7d2d1dec41e380e9ef435d21bdbc9eccb513c1c113a6319722abaec1720353"
+  sha256 "75c0e95c4202396bf662e0b257325f1b906f0d8e1a072f242aa4815c1fa8ebfe"
 
   depends_on "fzf"
   depends_on "git-delta" => :recommended
+  depends_on "git" => :recommended
 
   def install
     bin.install "gdi"
