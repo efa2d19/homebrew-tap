96effd8251640cad4cf632f3f392069574e643f1
efa2d19 <44712637+efa2d19@users.noreply.github.com>
2025-07-02 01:07:00 +0400

    chore(fzf-git-ext): updated to 1.4.0
    also moved git-delta dep to required

--- Formula/fzf-git-extensions.rb
+++ Formula/fzf-git-extensions.rb
@@ -1,12 +1,12 @@
 class FzfGitExtensions < Formula
-  version "1.3.2"
+  version "1.4.0"
   desc "Quick Git actions using fzf"
   homepage "https://github.com/efa2d19/fzf-git-extensions"
   url "https://github.com/efa2d19/fzf-git-extensions/archive/refs/tags/#{version}.tar.gz"
-  sha256 "195d6ef22bd97ef03e7c42135b89f71010972670895f67578b8181ad8af38f76"
+  sha256 "e5aa6c99afa1bb9cf1d5db0bc7ea49d796de071566c662f9d59037ea63e27583"
 
   depends_on "fzf"
-  depends_on "git-delta" => :recommended
+  depends_on "git-delta"
   depends_on "git" => :recommended
 
   def install
